6163ea75729d25a723aa92604b58151bbea51bc9
YingLuo111 <47013288+YingLuo111@users.noreply.github.com>
2021-11-28 16:33:41 -0800

    Add blackbox nav tests

--- Fakeflix/src/tests/blackbox_testing/Play_Video_Tests/src/com/CPSC542/blackboxTesting/playVideo/PlayVideoTestDriver.java
+++ Fakeflix/src/tests/blackbox_testing/Play_Video_Tests/src/com/CPSC542/blackboxTesting/playVideo/PlayVideoTestDriver.java
@@ -1,27 +1,93 @@
 package com.CPSC542.blackboxTesting.playVideo;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class PlayVideoTestDriver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", "chromedriver_mac64");
 		
 		WebDriver driver = new ChromeDriver();
-		
-		driver.get("https://www.google.com");
+	
 		try {
-			Thread.sleep(2000);
+			//launch Fakeflix
+			driver.get("http://localhost:3000");
+			driver.manage().window().maximize();
+			log("Fakeflix started");
+			sleep(1);
+			
+			/* log in with account */
+			//set user email
+			driver.findElement(By.name("email")).sendKeys("cpsc542_fakeflix_blackbox_tests@gmail.com");
+			log("Seting email for login");
+			sleep(1);
+			
+			//set user password
+			driver.findElement(By.name("password")).sendKeys("blackbox_tests");
+			log("Seting password for login");
+			sleep(1);
+			
+			//click login button
+			driver.findElement(By.className("SignIn__form--button")).click();
+			log("logging in");
+			sleep(10);
+			
+			/* navigation functional tests */
+			//nav with Mylist tab
+			driver.findElement(By.xpath("//a[@href='/mylist']")).click();
+			log("Nav with Mylist tab");
+			sleep(3);
+			
+			//nav with News & Popular tab
+			driver.findElement(By.xpath("//a[@href='/popular']")).click();
+			log("Nav with News & Popular tab");
+			sleep(3);
+
+			//nav with Movies tab
+			driver.findElement(By.xpath("//a[@href='/movies']")).click();
+			log("Nav with Movies tab");
+			sleep(3);
+			
+			//nav with TV Series tab
+			driver.findElement(By.xpath("//a[@href='/tvseries']")).click();
+			log("Nav with Series tab");
+			sleep(3);
+			
+			//nav with Home tab
+			driver.findElement(By.xpath("//a[@href='/browse']")).click();
+			log("Nav with Home tab");
+			sleep(3);
+
+			//nav with Fakeflix icon
+			driver.findElement(By.xpath("//a[@href='/']")).click();
+			log("Nav with Fakeflix icon");
+			sleep(10);
+			
+			log("Navigation tests succeeded");
+		} catch (Exception e) {
+			log("Navigation tests failed");
+		} finally {
+			driver.close();
+		}
+		
+	}
+	
+	private static void sleep(int timeInSeconds) {
+		try {
+			Thread.sleep(timeInSeconds * 1000);
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println(driver.getTitle());
-		
-		driver.close();
-		
+	}
+	
+	private static void log(String message) {
+		System.out.println(message);
 	}
 
 }
