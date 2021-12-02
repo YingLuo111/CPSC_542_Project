778f2d7a47b52249661a4749868c98457e547f61
YingLuo111 <47013288+YingLuo111@users.noreply.github.com>
2021-12-02 01:13:20 -0800

    Add unit test for Banner component

--- Fakeflix/src/tests/whitebox_testing/Banner.spec.js
+++ Fakeflix/src/tests/whitebox_testing/Banner.spec.js
@@ -1,24 +1,130 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { expect } from 'chai';
-// import { configure, shallow } from 'enzyme';
-// import { spy } from 'sinon';
-// import Foo from './src/Foo';
-// import { useDispatch, useSelector } from "react-redux";
+import React from 'react';
+import { configure, mount } from 'enzyme';
+import { expect } from 'chai';
+import Banner from '../../components/Banner/Banner';
+import Adapter from 'enzyme-adapter-react-16'
+import * as redux from 'react-redux'
+import * as sinon from 'sinon';
+import * as actions from "../../redux/modal/modal.actions";
+import * as movieSelectors from "../../redux/movies/movies.selectors";
+import * as seriesSelectors from "../../redux/series/series.selectors";
+import * as utils from "../../utils";
+import { Provider } from 'react-redux'
+import { StaticRouter } from "react-router-dom"
+import configureStore from 'redux-mock-store'
+import { JSDOM } from 'jsdom';
 
-// import Adapter from 'enzyme-adapter-react-16'
-// configure({ adapter: new Adapter() });
+const { window } = new JSDOM('<!doctype html><html><body></body></html>');
 
-// describe('unit test for Banner', () => {
+function copyProps(src, target) {
+  const props = Object.getOwnPropertyNames(src)
+    .filter(prop => typeof target[prop] === 'undefined')
+    .reduce((result, prop) => ({
+      ...result,
+      [prop]: Object.getOwnPropertyDescriptor(src, prop),
+    }), {});
+  Object.defineProperties(target, props);
+}
 
-//     beforeEach(function() {
-//         sinon.spy('react-redux', );
-//       });
+global.window = window;
+global.document = window.document;
+global.navigator = {
+  userAgent: 'node.js',
+};
 
-//     it('fires a login request action', () =>
-//       store
-//         .dispatch(login(
-//           { username: 'user', password: 'pass' },
-//           mockServiceCreator(REQUIRED_BODY),
-//         ))
-//         .then(() => expect(store.getActions()).toContainEqual({ type: LOGIN_REQUEST })));
+copyProps(window, global);
+
+configure({ adapter: new Adapter() });
+
+describe('Banner component testing', function() {
+
+    let dummyDataFromIMDB = {
+        data: [
+            {
+                adult: false,
+                backdrop_path: "/70nxSw3mFBsGmtkvcs91PbjerwD.jpg",
+                genre_ids: [878,28,12],
+                id: 580489,
+                isFavorite: false,
+                media_type: "movie",
+                original_language: "en",
+                original_title: "Venom: Let There Be Carnage",
+                overview: "After finding a host body in investigative reporter Eddie Brock, the alien symbiote must face a new enemy, Carnage, the alter ego of serial killer Cletus Kasady.",
+                popularity: 13071.277,
+                poster_path: "/rjkmN1dniUHVYAtwuV3Tji7FsDO.jpg",
+                release_date: "2021-09-30",
+                title: "Venom: Let There Be Carnage",
+                video: false,
+                vote_average: 7.2,
+                vote_count: 3726
+            }
+        ],
+        error: "",
+        loading: false
+    }
+       
+    var useSelectorStub, randomStub, useDispatchSpy, showModalDetailStub, selectTrendingMoviesStub, selectNetflixMoviesStub, selectNetflixSeriesStub;
+    const initialState = {output:10}
+    const mockStore = configureStore()
+    let store
+    beforeEach(function() {
+        let dummyDispatch = sinon.spy();
+
+        useSelectorStub = sinon.stub(redux, 'useSelector');
+        useSelectorStub.returns(dummyDataFromIMDB);
+
+        randomStub = sinon.stub(utils, "randomize");
+        randomStub.returns(0);
+
+        selectTrendingMoviesStub = sinon.stub(movieSelectors, 'selectTrendingMovies');
+        selectTrendingMoviesStub.returns(dummyDataFromIMDB);
+
+        selectNetflixMoviesStub = sinon.stub(movieSelectors, 'selectNetflixMovies');
+        selectNetflixMoviesStub.returns(dummyDataFromIMDB);
+
+        selectNetflixSeriesStub = sinon.stub(seriesSelectors, 'selectNetflixSeries');
+        selectNetflixSeriesStub.returns(dummyDataFromIMDB);
+
+        useDispatchSpy = sinon.spy(redux, 'useDispatch');
+        useDispatchSpy.returned(dummyDispatch);
+
+        showModalDetailStub = sinon.stub(actions, 'showModalDetail');
+        store = mockStore(initialState)
+    });
+
+    afterEach(function() {
+        useSelectorStub.restore();
+        useDispatchSpy.restore();
+        showModalDetailStub.restore();
+        selectTrendingMoviesStub.restore();
+        selectNetflixMoviesStub.restore();
+        selectNetflixSeriesStub.restore();
+        randomStub.restore();
+    });
+
+    it('Create movie banner', function() {
+      const wrapper = mount(<StaticRouter><Provider store={store}><Banner type="movies" /></Provider></StaticRouter>); 
+    //   console.log(wrapper.debug());
+      expect(wrapper.find(".Banner")).to.have.length.least(1);
+      expect(wrapper.find(".Banner__content")).to.have.length.least(1);
+      expect(wrapper.find(".Banner__content--title")).to.have.length.least(1);
+      expect(wrapper.find(".Banner__buttons")).to.have.length.least(1);
+      expect(wrapper.find(".Banner__button")).to.have.length.least(1);
+      expect(wrapper.find(".Banner__content--description")).to.have.length.least(1);
+      expect(wrapper.find(".Banner__panel")).to.have.length.least(1);
+      expect(wrapper.find(".Banner__bottom-shadow")).to.have.length.least(1);
+    });
+
+    it('Create tv banner', function() {
+        const wrapper = mount(<StaticRouter><Provider store={store}><Banner type="series" /></Provider></StaticRouter>); 
+        // console.log(wrapper.debug());
+        expect(wrapper.find(".Banner")).to.have.length.least(1);
+        expect(wrapper.find(".Banner__content")).to.have.length.least(1);
+        expect(wrapper.find(".Banner__content--title")).to.have.length.least(1);
+        expect(wrapper.find(".Banner__buttons")).to.have.length.least(1);
+        expect(wrapper.find(".Banner__button")).to.have.length.least(1);
+        expect(wrapper.find(".Banner__content--description")).to.have.length.least(1);
+        expect(wrapper.find(".Banner__panel")).to.have.length.least(1);
+        expect(wrapper.find(".Banner__bottom-shadow")).to.have.length.least(1);
+      });
+  });
